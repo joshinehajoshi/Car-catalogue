d6a12bd9b860671fc4ac9793be2c85bfaf551eb4
Neha Joshi <nehajoshigkp@gmail.com>
2022-01-23 00:03:03 +0530

    img added

--- src/Components/Catalogue.js
+++ src/Components/Catalogue.js
@@ -22,11 +22,11 @@ function Catalogue() {
       <div className="catalogue-wrapper">
       {CarData.map((a) => {
           return (
-              <div >
+              <div className="catalogue-card">
             <Card style={{ width: '18rem' }}>
-            <Card.Img variant="top" src="holder.js/100px180?text=Image cap" />
+            <Card.Img variant="top" src="https://i.ytimg.com/vi/dip_8dmrcaU/maxresdefault.jpg" />
             <Card.Body>
-              <Card.Title>Card Title</Card.Title>
+              <Card.Title>{a.Name}</Card.Title>
               <Card.Text>
                 Some quick example text to build on the card title and make up the bulk of
                 the card's content.

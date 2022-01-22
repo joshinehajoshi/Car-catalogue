0b6030d34cf7aaa9771bef7a59fef1f9e97f82aa
Neha Joshi <nehajoshigkp@gmail.com>
2022-01-22 15:54:55 +0530

    routing

---
+++ src/Components/Contact.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Contact() {
+  return (
+  <div >
+      <h1>Contact</h1>
+      <h1>Contact</h1>
+      <h1>Contact</h1>
+      <h1>Contact</h1>
+      <h1>Contact</h1>
+      
+  </div>
+  )
+}
+
+export default Contact;
